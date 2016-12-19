1c4a280a259e08a3356e11c645012f9cf27e6652
Frederick Bejarano <frederick.bejarano@almundo.com>
2016-12-19 14:56:28 -0300

    add format for chrome warnings

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ module.exports = function() {
 	// create a stream through which each file will pass
 	return through.obj(function(file, enc, callback) {
 
+		var formats = {
+			'.ttf': 'truetype',
+			'.woff': 'woff',
+			'.svg': 'svg',
+			'.eot': 'embedded-opentype'
+		};
+
 		if (file.isNull()) {
 			this.push(file);
 			// do nothing if no contents
@@ -25,11 +32,13 @@ module.exports = function() {
 		if (file.isBuffer()) {
 			var file64 = new Buffer(file.contents).toString('base64');
 			var mtype =  mime.lookup(file.path);
-			var fileext = path.extname(file.path); 
+			var fileext = path.extname(file.path);
+			var format = formats[fileext];
 			var filename = path.basename(file.path, fileext);
-			var csswrapper = '@font-face {font-family: '+filename+'; src: url(data:'+mtype+';base64,'+file64+');}';
+			var csswrapper = '@font-face {font-family: '+filename+'; src: url(data:'+mtype+';base64,'+file64+') format("'+ format +'");}';
 			var output = csswrapper;
-			
+			console.log(csswrapper);
+
 			file.contents = new Buffer(output);
 			file.path = gutil.replaceExtension(file.path, '.css');
 			return callback(null, file);
