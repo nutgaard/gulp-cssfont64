505d2853270f9db505cc5588b094883c6bd052ad
Nicklas Utgaard <nicklas.utgaard@bekk.no>
2017-02-28 14:47:56 +0100

    Allow custom css-formatter to be sent in as option

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var formats = {
     '.eot': 'embedded-opentype'
 };
 
-module.exports = function () {
+module.exports = function (formatter) {
     // create a stream through which each file will pass
     return through.obj(function (file, enc, callback) {
         if (file.isNull()) {
@@ -32,10 +32,15 @@ module.exports = function () {
             var fileext = path.extname(file.path);
             var format = formats[fileext];
             var filename = path.basename(file.path, fileext);
-            var csswrapper = '@font-face {font-family: ' + filename + '; src: url(data:' + mtype + ';base64,' + file64 + ') format("' + format + '");}';
-            var output = csswrapper;
 
-            file.contents = new Buffer(output);
+            var csswrapper = '';
+            if (formatter) {
+                csswrapper = formatter(filename, mtype, file64, format);
+            } else {
+                csswrapper = '@font-face {font-family: ' + filename + '; src: url(data:' + mtype + ';base64,' + file64 + ') format("' + format + '");}';
+            }
+
+            file.contents = new Buffer(csswrapper);
             file.path = gutil.replaceExtension(file.path, '.css');
             return callback(null, file);
         }
