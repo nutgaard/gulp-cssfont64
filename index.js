0718683d8994644a97b2e540b767e92aecd4d83c
Nicklas Utgaard <nutgaard@gmail.com>
2017-03-09 20:15:38 +0100

    Allow consumer to pass in a custom csswrapper

--- index.js
+++ index.js
@@ -5,19 +5,16 @@ var fs = require('fs');
 var path = require('path');
 var mime = require('mime');
 
-module.exports = function() {
+var formats = {
+    '.ttf': 'truetype',
+    '.woff': 'woff',
+    '.svg': 'svg',
+    '.eot': 'embedded-opentype'
+};
 
-
+module.exports = function(formatter) {
 	// create a stream through which each file will pass
 	return through.obj(function(file, enc, callback) {
-
-		var formats = {
-			'.ttf': 'truetype',
-			'.woff': 'woff',
-			'.svg': 'svg',
-			'.eot': 'embedded-opentype'
-		};
-
 		if (file.isNull()) {
 			this.push(file);
 			// do nothing if no contents
@@ -35,10 +32,15 @@ module.exports = function() {
 			var fileext = path.extname(file.path);
 			var format = formats[fileext];
 			var filename = path.basename(file.path, fileext);
-			var csswrapper = '@font-face {font-family: '+filename+'; src: url(data:'+mtype+';base64,'+file64+') format("'+ format +'");}';
-			var output = csswrapper;
+			
+			var csswrapper = '';
+			if (formatter) {
+				csswrapper = formatter(filename, mtype, file64, format);
+			} else {
+				csswrapper = '@font-face {font-family: '+filename+'; src: url(data:'+mtype+';base64,'+file64+') format("'+ format +'");}';
+			}
 
-			file.contents = new Buffer(output);
+			file.contents = new Buffer(csswrapper);
 			file.path = gutil.replaceExtension(file.path, '.css');
 			return callback(null, file);
 		}
